39270c06db7962c01cf4a386cb62304d1defb7da
rofl0r <rofl0r@users.noreply.github.com>
2021-08-27 05:03:41 +0000

    add new libao audio backend
    2 decades ago, when gnuboy was written, OSS was the main audio backend
    for most unices, though it's been replaced with alsa on linux since, so
    on a vanilla kernel configuration no sound comes out of the speakers
    with gnuboy's default audio backend (except for SDL variant).
    unfortunately, using alsa's libasound is quite complex and involves lots
    of code.
    libao is a portable audio layer, which, on linux, sits on top of alsa
    and provides a very simple and nice API, very similar to what OSS used
    to be.
    ao is now selected by default when --with-sound=yes (default) is passed
    to configure, as it's superior to the mostly-defunct OSS backend.

---
+++ sys/ao/ao.c
@@ -0,0 +1,89 @@
+#undef _GNU_SOURCE
+#define _GNU_SOURCE
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <ao/ao.h>
+
+#include "defs.h"
+#include "pcm.h"
+#include "rc.h"
+
+struct pcm pcm;
+
+static int stereo = 1;
+static int samplerate = 44100;
+static int sound = 1;
+static ao_device *device;
+static ao_sample_format format;
+static int aodriver;
+
+
+rcvar_t pcm_exports[] =
+{
+	RCV_BOOL("sound", &sound),
+	RCV_INT("stereo", &stereo),
+	RCV_INT("samplerate", &samplerate),
+	RCV_END
+};
+
+
+static void no_sound(void) {
+	pcm.hz = 11025;
+	pcm.len = 4096;
+	pcm.buf = malloc(pcm.len);
+	pcm.pos = 0;
+}
+
+void pcm_init()
+{
+	if (!sound)
+	{
+		no_sound();
+		return;
+	}
+
+	ao_initialize();
+	format.bits = 8;
+	format.channels = 2;
+	format.rate = samplerate;
+	format.byte_format = AO_FMT_LITTLE;
+	aodriver = ao_default_driver_id();
+	device = ao_open_live(aodriver, &format, NULL);
+
+	if(!device) {
+		no_sound();
+		return;
+	}
+
+	pcm.stereo = 1;
+	pcm.hz = samplerate;
+	pcm.len = samplerate / 60;
+	pcm.buf = malloc(pcm.len);
+}
+
+void pcm_close()
+{
+	if (pcm.buf) free(pcm.buf);
+	memset(&pcm, 0, sizeof pcm);
+	if(device) ao_close(device);
+}
+
+int pcm_submit()
+{
+	if (!device)
+	{
+		pcm.pos = 0;
+		return 0;
+	}
+	if (pcm.buf) ao_play(device, pcm.buf, pcm.pos);
+	pcm.pos = 0;
+	return 1;
+}
