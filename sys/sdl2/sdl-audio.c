cc5726edbda706b59e583b9a1a5a691c5823f40f
rofl0r <rofl0r@users.noreply.github.com>
2022-03-28 18:44:15 +0000

    sdl2 audio: add sound_threaded rc var
    since most sdl users don't use the new blocking API, it's not as
    well-tested and the old callback thread method may work better
    on some platforms.

--- sys/sdl2/sdl-audio.c
+++ sys/sdl2/sdl-audio.c
@@ -24,14 +24,24 @@ static int samplerate = 44100;
 static int stereo = 1;
 static SDL_AudioDeviceID device;
 static int paused;
+static int threaded;
+static volatile int audio_done;
 
 rcvar_t pcm_exports[] =
 {
 	RCV_BOOL("sound", &sound, "enable sound"),
+	RCV_BOOL("sound_threaded", &threaded, "use threaded sound"),
 	RCV_INT("stereo", &stereo, "enable stereo"),
 	RCV_INT("samplerate", &samplerate, "samplerate, recommended: 32768"),
 	RCV_END
 };
+
+
+static void audio_callback(void *blah, byte *stream, int len)
+{
+	memcpy(stream, pcm.buf, len);
+	audio_done = 1;
+}
 
 void pcm_init()
 {
@@ -48,7 +58,7 @@ void pcm_init()
 	as.userdata = 0;
 	for (i = 1; i < as.samples; i<<=1);
 	as.samples = i;
-	as.callback = NULL;
+	as.callback = threaded ? audio_callback : NULL;
 	device = SDL_OpenAudioDevice(NULL, 0, &as, &ob, SDL_AUDIO_ALLOW_CHANNELS_CHANGE|SDL_AUDIO_ALLOW_FREQUENCY_CHANGE);
 	if (!device) {
 		sound = 0;
@@ -65,11 +75,19 @@ void pcm_init()
 
 int pcm_submit()
 {
-	int res, min = pcm.len*2;
+	int res, min;
 	if (!sound || !pcm.buf || paused) {
 		pcm.pos = 0;
 		return 0;
 	}
+	if(threaded) {
+		if(pcm.pos < pcm.len) return 1;
+		while(!audio_done) SDL_Delay(4);
+		audio_done = 0;
+		pcm.pos = 0;
+		return 1;
+	}
+	min = pcm.len*2;
 	res = SDL_QueueAudio(device, pcm.buf, pcm.pos) == 0;
 	pcm.pos = 0;
 	while (res && SDL_GetQueuedAudioSize(device) > min)
